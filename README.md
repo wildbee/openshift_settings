220c9c2a75f6313163d3887e375e5a0fdbb1a96d
Dustin Kut Moy Cheung <dcheung@redhat.com>
2013-11-12 15:50:00 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ openshift_settings
 ==================
 
 This repo will contain the necessary settings to deploy the wildbee application to an openshift instance
+
+## Instructions
+```bash
+rhc create-app <project> diy-0.1
+cd <project>
+
+# add PostgreSQL cartridge
+rhc cartridge-add -a <project>  -c postgresql-9.2
+
+# get the openshift settings
+git clone https://github.com/wildbee/openshift_settings.git
+mv openshift_settings .openshift
+
+# push to openshift and run
+sh ./openshift/deploy.sh <path_of_wildbee_repo>
+```
---
+++ deploy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+
+# Script to compile and bundle your wildbee repo for distribution, add the
+# resulting zip produced to this repo, then upload it to openshift
+#
+# Usage: sh deploy,sh <path of local wildbee repo>
+#
+set -e
+
+WILDBEE_REPO="$1"
+SCRIPT="`readlink -e $0`"
+SCRIPTPATH="`dirname $SCRIPT`"
+REPOPATH="`dirname $SCRIPTPATH`"
+
+echo "Path of Wildbee repo:" $WILDBEE_REPO
+
+# Compile it
+echo "==========================="
+echo "Compiling your wildbee repo"
+echo "==========================="
+pushd $1
+play dist
+
+# copy the zip to openshift repository
+echo "====================================="
+echo "Copying the zip to the openshift repo"
+echo "====================================="
+cp target/universal wildbee-*\.zip $REPOPATH/wildbee.zip
+
+popd $1
+
+# unzip the zip, then delete it
+echo "========================="
+echo "Unzipping the wildbee zip"
+echo "========================="
+unzip wildbee.zip
+rm wildbee.zip
+
+# rename the folder produced by the zip to something that does not contain the
+# version
+echo "==========================="
+echo "Renaming the wildbee folder"
+echo "==========================="
+mv wildbee-* wildbee
+
+# add, commit and push to the openshift repo
+echo "=================================="
+echo "Commiting and pushing to openshift"
+echo "=================================="
+git add --all
+git commit -am 'Yet another update'
+git push
